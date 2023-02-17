260de33cb8842f7f1eca009885c5fa15a93c0284
oumao <evansotis2015@gmail.com>
2023-02-17 15:54:36 +0300

    Update todolist

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from 'src/app/services/todo.service';
-
+import { Todo } from 'src/app/models/Todo';
 
 @Component({
   selector: 'app-todolist',
@@ -8,6 +8,8 @@ import { TodoService } from 'src/app/services/todo.service';
   styleUrls: ['./todolist.component.css']
 })
 export class TodolistComponent implements OnInit{
+
+  todos = Todo[]
 
   constructor(private todoService: TodoService){}
 
