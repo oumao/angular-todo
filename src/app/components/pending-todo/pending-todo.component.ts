16e6525a750bd6b1d32d082ae3d2354033df6ec3
oumao <evansotis2015@gmail.com>
2023-02-18 21:15:15 +0300

    Added ngOnInit lifecycle

--- src/app/components/pending-todo/pending-todo.component.ts
+++ src/app/components/pending-todo/pending-todo.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pending-todo',
   templateUrl: './pending-todo.component.html',
   styleUrls: ['./pending-todo.component.css']
 })
-export class PendingTodoComponent {
+export class PendingTodoComponent implements OnInit {
 
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 }
