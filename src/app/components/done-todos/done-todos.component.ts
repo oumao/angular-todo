886328d8394c138d3db2b1559b852f9367c198fe
oumao <evansotis2015@gmail.com>
2023-02-18 17:14:20 +0300

    Done todos

---
+++ src/app/components/done-todos/done-todos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-done-todos',
+  templateUrl: './done-todos.component.html',
+  styleUrls: ['./done-todos.component.css']
+})
+export class DoneTodosComponent {
+
+}
