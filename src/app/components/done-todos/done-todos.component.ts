017e8207ef66fa8998295046355b8543631b6338
oumao <evansotis2015@gmail.com>
2023-02-19 00:29:46 +0300

    Query done todos for display from local storage

--- src/app/components/done-todos/done-todos.component.ts
+++ src/app/components/done-todos/done-todos.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { DoneTodosService } from 'src/app/services/done-todos.service'
+import { Todo } from '../../models/Todo'
 
 @Component({
   selector: 'app-done-todos',
   templateUrl: './done-todos.component.html',
   styleUrls: ['./done-todos.component.css']
 })
-export class DoneTodosComponent {
+export class DoneTodosComponent implements OnInit {
 
+  doneTodos: Todo[] = []
+
+  @Output() updatedTodos = new EventEmitter<Todo>();
+
+  constructor(private doneService: DoneTodosService){
+
+  }
+
+  ngOnInit(): void {
+    this.doneTodos = this.doneService.getToDones();
+  }
 }
